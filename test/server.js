43870e6304f470f3250ca5162281003c1580defc
Luca Peduto <luca@eloquentbit.com>
2017-08-17 08:04:29 +0200

    Add test to check if server is listening on specified port

--- test/server.js
+++ test/server.js
@@ -1,7 +1,31 @@
+const net = require('net')
 const assert = require('chai').assert
+const server = require('../server')
+
+const PORT = 10000
+
+beforeEach(function() {
+  server.listen({
+    host: '0.0.0.0',
+    port: PORT
+  })
+})
+
+afterEach(function() {
+  server.close()
+})
 
 describe('Basic setup', function() {
   it('should work', function() {
     assert.equal(true, true)
+  })
+})
+
+describe('Chat server', function() {
+  it('should listen on specified port', function(done) {
+    const client = net.createConnection({ port: PORT }, function() {
+      assert.isNotNull(client)
+      done()
+    })
   })
 })

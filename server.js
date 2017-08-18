84ea345e4a2661fd226cc551107eeee931fc5010
Luca Peduto <luca@eloquentbit.com>
2017-08-18 06:41:41 +0200

    Refactor server.js to use ConnectionManager library

--- server.js
+++ server.js
@@ -1,27 +1,16 @@
 'use strict'
 
 const net = require('net')
-
-// This array holds the connected sockets
-let sockets = {}
-
-// Register a socket in the sockets array
-function registerSocket(socket) {
-  sockets[socket.name] = socket
-}
+const ConnectionManager = require('./lib/ConnectionManager')
 
 // Parse the input and remove \n and \r characters
 function sanitizeInput(data) {
   return data.toString().replace(/\r\n|\n\r/gm, '')
 }
 
-// Return the list of currently connected sockets as a string
-function getConnectedClients() {
-  return Object.keys(sockets).join('\n')
-}
-
-// Send the specified message to all connected sockets
+// Send the specified message to all connected sockets except the sender
 function broadcastMessage(message, sender) {
+  const sockets = connectionManager.getAllSockets()
   Object.keys(sockets).forEach(socket => {
     const currentSocket = sockets[socket]
     if (currentSocket !== sender) {
@@ -30,37 +19,44 @@ function broadcastMessage(message, sender) {
   })
 }
 
+// Create a new ConnectionManager
+const connectionManager = new ConnectionManager()
+
 // Create a new server
 const server = net.createServer(socket => {
   // Add a name to the socket
   socket.name = `${socket.remoteAddress}:${socket.remotePort}`
 
+  console.log(`New client connected: ${socket.name}`)
+
   // Register the socket
-  registerSocket(socket)
+  connectionManager.registerSocket(socket)
 
   // Handle the data sent from socket
   socket.on('data', data => {
     const parsedData = sanitizeInput(data)
 
     if (parsedData === '@list') {
-      socket.write(`${getConnectedClients()}`)
+      socket.write(`${connectionManager.getConnectedSockets()}\n`)
+    } else if (parsedData === '@exit') {
+      connectionManager.removeSocket(socket)
+      socket.destroy()
     } else {
       broadcastMessage(data, socket)
     }
   })
 
-  // When socket disconnect delete it from the sockets array
-  socket.on('close', socket => {
-    delete sockets[socket.name]
+  // Remove socket on disconnect
+  socket.on('close', () => {
+    connectionManager.removeSocket(socket)
+    socket.destroy()
   })
 })
 
-// When server shutdwon close all sockets and remove them from sockets array
+// Remove all sockets on server shutdwon
 server.on('close', () => {
-  for (const socket in sockets) {
-    sockets[socket].destroy()
-    delete sockets[socket]
-  }
+  console.log('Server on close called')
+  connectionManager.removeAllSockets()
 })
 
 module.exports = server
